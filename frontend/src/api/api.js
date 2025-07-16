694a9b5f7d9c3843d6ba39a04b826c0621f90eb6
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-07-16 17:46:24 +0530

    setup the apis

---
+++ frontend/src/api/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/tasks`,
+});
+
+// Get all tasks
+export const fetchTask = () => API.get("/");
+
+// Add a task
+export const addTask = (taskData) => API.post("/", taskData)
+
+// Toggle task completion
+export const toggleTask = (id) => API.put(`/${id}`);
+
+// Delete task
+export const deleteTask = (id) => API.delete(`/${id}`);
